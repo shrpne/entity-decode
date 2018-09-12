78d3e97d1670aa933ae60c229d4a0a5fd44ed6e6
shrpne <shrpne@gmail.com>
2018-09-12 18:05:48 +0300

    kepp HTML tags in the output string in the browser implementation

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 2.0.0 - 2018-09-12
+- Keel HTML tags in the output string for browser implementation to align with server `he` behavior
+
+## 1.0.1 - 2018-05-30
+- Specify published to npm files
+
+## 1.0.0 - 2018-05-30
+- Initial 
--- browser.js
+++ browser.js
@@ -1,14 +1,18 @@
 /**
- * from https://github.com/vuejs/vue/commit/a855dd0564a657a73b7249469490d39817f27cf7#diff-c0a2623ea5896a83e3b630f236b47b52
+ * @see https://github.com/vuejs/vue/commit/a855dd0564a657a73b7249469490d39817f27cf7#diff-c0a2623ea5896a83e3b630f236b47b52
+ * @see https://stackoverflow.com/a/13091266/4936667
  */
 
 let decoder;
 
 function decode(html) {
     decoder = decoder || document.createElement('div');
+    // Escape HTML before decoding for HTML Entities
+    html = escape(html).replace(/%26/g,'&').replace(/%23/g,'#').replace(/%3B/g,';');
+    // decoding
     decoder.innerHTML = html;
 
-    return decoder.textContent;
+    return unescape(decoder.textContent);
 }
 
 module.exports = decode;
